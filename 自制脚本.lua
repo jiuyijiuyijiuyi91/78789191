ac8c2cc7b41e76ab8c8f91a3087fbbb9bce0a239
jiuyijiuyijiuyi91 <3793272518@qq.com>
2026-08-31 12:48:21 +0800

    Add files via upload

--- 自制脚本.lua
+++ 自制脚本.lua
@@ -3294,13 +3294,12 @@ end)
 ServerBox:AddButton("[FPS]一键点击", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/jiuyijiuyijiuyi91/78789191/refs/heads/main/FPS%E4%B8%80%E9%94%AE%E7%94%B5%E5%87%BB.lua"))()
 end)
-ServerBox:AddButton("捕捉10亿鸭子", function()
+ServerBox:AddButton("捕捉10亿只鸭子", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/jiuyijiuyijiuyi91/78789191/refs/heads/main/%E6%8D%95%E6%8D%8910%E4%BA%BF%E5%8F%AA%E9%B8%AD%E5%AD%90.lua"))()
 end)
 -- UI设置
 Tabs["UI Settings"] = Window:AddTab("UI设置", "settings")
-
-ThemeManager:SetLibrary(Library)
+Manager:SetLibrary(Library)
 SaveManager:SetLibrary(Library)
 
 SaveManager:IgnoreThemeSettings()
